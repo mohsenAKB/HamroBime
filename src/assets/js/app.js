8e9aad7fc1ffb06668457408b07af050d63e1b4a
unknown <mohsen.akb1377@gmail.com>
2023-05-27 17:56:12 +0330

    feat:add online service section and become responsive

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,22 @@
-import '../scss/app.scss';
+import "../scss/app.scss";
 
 /* Your JS Code goes here */
 
-import 'bootstrap/dist/js/bootstrap.bundle';
+import "bootstrap/dist/js/bootstrap.bundle";
+
+import Swal from "../../../node_modules/sweetalert2";
+const sendOrderBtn = document.querySelector("#send-order");
+sendOrderBtn.addEventListener("click", popupSendOrder);
+
+function popupSendOrder() {
+  Swal.fire({
+    icon: "info",
+    title: "انتقادات و پیشنهادات شما",
+    confirmButtonText: "ارسال",
+    showCloseButton: true,
+    html:
+      '<input id="swal-input1" placeholder="نام و نام خانوادگی*" class="swal2-input">' +
+      '<input id="swal-input2" placeholder="example@gmail.com" class="swal2-input">' +
+      '<input id="swal-input2" placeholder="توضیحات" class="swal2-input">',
+  });
+}
