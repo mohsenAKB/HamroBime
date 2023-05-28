e88d37bd1d8dce38b28a39c0322aa459538abdb4
unknown <mohsen.akb1377@gmail.com>
2023-05-28 20:29:18 +0330

    feat:add footer

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,8 +1,12 @@
 import "../scss/app.scss";
 
 /* Your JS Code goes here */
-
+import $ from "jquery";
 import "bootstrap/dist/js/bootstrap.bundle";
+import "../../../node_modules/slick-carousel/slick/slick";
+import "../../../node_modules/slick-carousel/slick/slick.css";
+import "../../../node_modules/slick-carousel/slick/slick-theme.css";
+
 
 import Swal from "../../../node_modules/sweetalert2";
 const sendOrderBtn = document.querySelector("#send-order");
@@ -20,3 +24,37 @@ function popupSendOrder() {
       '<input id="swal-input2" placeholder="توضیحات" class="swal2-input">',
   });
 }
+
+
+$(".service-cards").slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
