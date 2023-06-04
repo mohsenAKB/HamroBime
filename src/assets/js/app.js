bdd8924e62bc32472190fa41bf88d05aaf290c04
unknown <mohsen.akb1377@gmail.com>
2023-06-04 11:52:13 +0330

    refactor:remove slick slider and swiper is alternative

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,13 +3,10 @@ import "../scss/app.scss";
 /* Your JS Code goes here */
 import $ from "jquery";
 import "bootstrap/dist/js/bootstrap.bundle";
-import "../../../node_modules/slick-carousel/slick/slick";
-import "../../../node_modules/slick-carousel/slick/slick.css";
-import "../../../node_modules/slick-carousel/slick/slick-theme.css";
-import { gsap } from "gsap";
-import { PixiPlugin } from "gsap/PixiPlugin.js";
-import { MotionPathPlugin } from "gsap/MotionPathPlugin.js";
-
+// import Swiper JS
+import Swiper from "swiper";
+// import Swiper styles
+import "swiper/css";
 import Swal from "../../../node_modules/sweetalert2";
 const sendOrderBtn = document.querySelector("#send-order");
 sendOrderBtn.addEventListener("click", popupSendOrder);
@@ -27,36 +24,11 @@ function popupSendOrder() {
   });
 }
 
-
-$(".service-cards").slick({
-  dots: true,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true,
+    const swiper = new Swiper(".mySwiper", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
       },
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-      },
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-  ],
-});
+    });
