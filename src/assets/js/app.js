4e645b31b1e129d94d6df3fb44ba7aa7e740ed16
unknown <mohsen.akb1377@gmail.com>
2023-06-06 12:38:20 +0330

    style: responsive header and add popup to btn in header

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -11,8 +11,13 @@ import Swiper from "swiper";
 import "swiper/css";
 import Swal from "../../../node_modules/sweetalert2";
 const sendOrderBtn = document.querySelector("#send-order");
-sendOrderBtn.addEventListener("click", popupSendOrder);
+const searchBox = document.getElementById("search-box");
+const headerSearch = document.querySelector(".header__search");
+const headerSearchBox = document.querySelector(".header__search-container-box");
+const btnCloseSearchBox = document.querySelector(".btn-close");
+const signinBtn = document.querySelector(".header__signin-btn");
 
+sendOrderBtn.addEventListener("click", popupSendOrder);
 function popupSendOrder() {
   Swal.fire({
     icon: "info",
@@ -25,6 +30,17 @@ function popupSendOrder() {
       '<input id="swal-input2" placeholder="توضیحات" class="swal2-input">',
   });
 }
+signinBtn.addEventListener("click", popupsigninBtn);
+function popupsigninBtn() {
+  Swal.fire({
+    title: "ورود و ثبت نام",
+    confirmButtonText: "ورود",
+    showCloseButton: true,
+    html:
+      '<input  type:tel placeholder="09905779530" class="swal2-input">' 
+  });
+}
+
 const swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   spaceBetween: 30,
@@ -42,81 +58,86 @@ const insuranceItemsBox = document.querySelectorAll(
 
 function headerAnimation() {
   const TL = gsap.timeline();
-  TL
-    .from(
-      navbarItem,
-      {
-        autoAlpha: 0,
-        stagger: 0.2,
-      },
-      "-=.5"
-    )
-    .from(insuranceItemsBox, {
+  TL.from(
+    navbarItem,
+    {
       autoAlpha: 0,
-      y: -70,
-      duration: 1,
       stagger: 0.2,
-    });
+    },
+    "-=.5"
+  ).from(insuranceItemsBox, {
+    autoAlpha: 0,
+    y: -70,
+    duration: 1,
+    stagger: 0.2,
+  });
 }
 
+const menu = document.querySelector(".menu");
+const menuMain = menu.querySelector(".menu-main");
+const goBack = menu.querySelector(".go-back");
+const menuTrigger = document.querySelector(".mobile-menu-trigger");
+const closeMenu = menu.querySelector(".mobile-menu-close");
+let subMenu;
+menuMain.addEventListener("click", (e) => {
+  if (!menu.classList.contains("active")) {
+    return;
+  }
+  if (e.target.closest(".menu-item-has-children")) {
+    const hasChildren = e.target.closest(".menu-item-has-children");
+    showSubMenu(hasChildren);
+  }
+});
+goBack.addEventListener("click", () => {
+  hideSubMenu();
+});
+menuTrigger.addEventListener("click", () => {
+  toggleMenu();
+});
+closeMenu.addEventListener("click", () => {
+  toggleMenu();
+});
+document.querySelector(".menu-overlay").addEventListener("click", () => {
+  toggleMenu();
+});
+function toggleMenu() {
+  menu.classList.toggle("active");
+  document.querySelector(".menu-overlay").classList.toggle("active");
+}
+function showSubMenu(hasChildren) {
+  subMenu = hasChildren.querySelector(".sub-menu");
+  subMenu.classList.add("active");
+  subMenu.style.animation = "slideLeft 0.5s ease forwards";
+  const menuTitle =
+    hasChildren.querySelector("i").parentNode.childNodes[0].textContent;
+  menu.querySelector(".current-menu-title").innerHTML = menuTitle;
+  menu.querySelector(".mobile-menu-head").classList.add("active");
+}
 
+function hideSubMenu() {
+  subMenu.style.animation = "slideRight 0.5s ease forwards";
+  setTimeout(() => {
+    subMenu.classList.remove("active");
+  }, 300);
+  menu.querySelector(".current-menu-title").innerHTML = "";
+  menu.querySelector(".mobile-menu-head").classList.remove("active");
+}
 
+window.onresize = function () {
+  if (this.innerWidth > 991) {
+    if (menu.classList.contains("active")) {
+      toggleMenu();
+    }
+  }
+};
 
- const menu = document.querySelector(".menu");
- const menuMain = menu.querySelector(".menu-main");
- const goBack = menu.querySelector(".go-back");
- const menuTrigger = document.querySelector(".mobile-menu-trigger");
- const closeMenu = menu.querySelector(".mobile-menu-close");
- let subMenu;
- menuMain.addEventListener("click", (e) => {
-   if (!menu.classList.contains("active")) {
-     return;
-   }
-   if (e.target.closest(".menu-item-has-children")) {
-     const hasChildren = e.target.closest(".menu-item-has-children");
-     showSubMenu(hasChildren);
-   }
- });
- goBack.addEventListener("click", () => {
-   hideSubMenu();
- });
- menuTrigger.addEventListener("click", () => {
-   toggleMenu();
- });
- closeMenu.addEventListener("click", () => {
-   toggleMenu();
- });
- document.querySelector(".menu-overlay").addEventListener("click", () => {
-   toggleMenu();
- });
- function toggleMenu() {
-   menu.classList.toggle("active");
-   document.querySelector(".menu-overlay").classList.toggle("active");
- }
- function showSubMenu(hasChildren) {
-   subMenu = hasChildren.querySelector(".sub-menu");
-   subMenu.classList.add("active");
-   subMenu.style.animation = "slideLeft 0.5s ease forwards";
-   const menuTitle =
-     hasChildren.querySelector("i").parentNode.childNodes[0].textContent;
-   menu.querySelector(".current-menu-title").innerHTML = menuTitle;
-   menu.querySelector(".mobile-menu-head").classList.add("active");
- }
-
- function hideSubMenu() {
-   subMenu.style.animation = "slideRight 0.5s ease forwards";
-   setTimeout(() => {
-     subMenu.classList.remove("active");
-   }, 300);
-   menu.querySelector(".current-menu-title").innerHTML = "";
-   menu.querySelector(".mobile-menu-head").classList.remove("active");
- }
-
- window.onresize = function () {
-   if (this.innerWidth > 991) {
-     if (menu.classList.contains("active")) {
-       toggleMenu();
-     }
-   }
- };
-
+function doFade() {
+  const tl = gsap.timeline();
+  const searchBox = document.querySelector(".header__search-box");
+  tl.to(searchBox, { display: "flex", duration: 0.5 });
+}
+headerSearch.addEventListener("click", () => {
+  headerSearchBox.classList.toggle("d-flex");
+  searchBox.classList.toggle("d-block");
+  doFade();
+});
